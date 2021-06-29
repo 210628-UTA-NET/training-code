e2781fa6872cd858a195d019d5af3a4a921e2ccd
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-29 13:08:13 -0500

    Added RRModels

---
+++ CoreC#/RestaurantReview/RRModels/Restaurant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RRModel
+{
+    public class Restaurant
+    {
+        private string _city;
+        public Restaurant()
+        { }
+        public string Name { get; set; }
+        public string City {
+
+        get; 
+
+            set
+            {
+                if (!Regex.isMatch(value, "@^[A-Za-z .]+$"))
+                {
+                    throw new Exception("City can only hold letters!");
+                }
+
+                _city = value;
+            }
+
+        }
+        public string State { get; set; }
+
+        public List<Review> Reviews { get; set; }
+    }
+}
---
+++ CoreC#/RestaurantReview/RRModels/Review.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RRModel
+{
+    public class Review
+    {
+        public Review()
+        { }
+
+        public double Rating { get; set; }
+
+        public string Description { get; set; }
+    }
+}
