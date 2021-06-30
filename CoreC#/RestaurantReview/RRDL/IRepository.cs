bcc466a59823bd6d959fd330a806a4e1f9fe91b8
Scifiler-sys <pagdilaostephen@gmail.com>
2021-06-30 11:30:14 -0500

    Added RRDL

---
+++ CoreC#/RestaurantReview/RRDL/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using RRModel;
+
+namespace RRDL
+{
+    /// <summary>
+    /// It is responsible for accessing our database (in this case it will be a JSON file stored in our folder)
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Gets a list of Restaurants stored in our database
+        /// </summary>
+        /// <returns>Returns a list of Restaurant</returns>
+        List<Restaurant> GetAllRestaurant();
+
+        /// <summary>
+        /// It will get a specific restaurant
+        /// </summary>
+        /// <param name="p_rest">This restaurant object will be used to check the properties that should match in the database</param>
+        /// <returns>Will return the restaurant object from the database</returns>
+        Restaurant GetRestaurant(Restaurant p_rest);
+
+        /// <summary>
+        /// It will ad a restaurant in our database
+        /// </summary>
+        /// <param name="p_rest">This is the restaurant object that will be added to the database</param>
+        /// <returns>Will return the restaurant object we just added</returns>
+        Restaurant AddRestaurant(Restaurant p_rest);
+    }
+}
---
+++ CoreC#/RestaurantReview/RRDL/Repository.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using RRModel;
+
+namespace RRDL
+{
+    public class Repository : IRepository
+    {
+        private const string _filePath = "./Database/Restaurant.json";
+        private string _jsonString;
+        public Restaurant AddRestaurant(Restaurant p_rest)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Restaurant> GetAllRestaurant()
+        {
+            try
+            {
+                _jsonString = File.ReadAllText(_filePath);
+            }
+            catch (System.Exception)
+            {
+                throw new Exception("File path is invalid");
+            }
+
+            //This will return a list of restaurant from the jsonString that came from 
+            return JsonSerializer.Deserialize<List<Restaurant>>(_jsonString);
+        }
+
+        public Restaurant GetRestaurant(Restaurant p_rest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
