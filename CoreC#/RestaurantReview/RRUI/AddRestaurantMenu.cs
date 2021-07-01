8cf7283c0985fa7e35983264cea52fb8105ed058
Scifiler-sys <pagdilaostephen@gmail.com>
2021-07-01 11:28:47 -0500

    Added AddRest menu

--- CoreC#/RestaurantReview/RRUI/AddRestaurantMenu.cs
+++ CoreC#/RestaurantReview/RRUI/AddRestaurantMenu.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Threading;
+using RRBL;
+using RRModel;
 
 namespace RRUI
 {
     public class AddRestaurantMenu : IMenu
     {
+        private static Restaurant _newRest = new Restaurant();
+        private IRestaurantBL _restBL;
+        public AddRestaurantMenu(IRestaurantBL p_restBL)
+        {
+            _restBL = p_restBL;
+        }
         public void Menu()
         {
             Console.WriteLine();
@@ -16,9 +25,9 @@ namespace RRUI
             Console.WriteLine(@"| $$  | $$|  $$$$$$$|  $$$$$$$");
             Console.WriteLine(@"|__/  |__/ \_______/ \_______/");
             Console.WriteLine();
-            Console.WriteLine("[4] Name");
-            Console.WriteLine("[3] City");
-            Console.WriteLine("[2] State");
+            Console.WriteLine("[4] Name - "+_newRest.Name);
+            Console.WriteLine("[3] City - "+ _newRest.City);
+            Console.WriteLine("[2] State - "+_newRest.State);
             Console.WriteLine("[1] Add Restaurant");
             Console.WriteLine("[0] Go back");
         }
@@ -32,13 +41,25 @@ namespace RRUI
                 case "0":
                     return MenuType.RestaurantMenu;
                 case "1":
+                    _restBL.AddRestaurant(_newRest);
                     return MenuType.RestaurantMenu;
                 case "2":
-                    return MenuType.RestaurantMenu;
+                    _newRest.State = Console.ReadLine();
+                    return MenuType.AddRestaurantMenu;
                 case "3":
-                    return MenuType.RestaurantMenu;
+                    try
+                    {
+                        _newRest.City = Console.ReadLine();
+                    }
+                    catch (System.Exception)
+                    {
+                        Console.WriteLine("City cannot hold numbers!");
+                        Thread.Sleep(1000);
+                    }
+                    return MenuType.AddRestaurantMenu;
                 case "4":
-                    return MenuType.RestaurantMenu;
+                    _newRest.Name = Console.ReadLine();
+                    return MenuType.AddRestaurantMenu;
                 default:
                     Console.WriteLine("Input was not correct");
                     Console.WriteLine("Press Enter to continue");
