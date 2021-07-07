7e76f39b6efe651472b809ff281395b31cfa11de
Scifiler-sys <pagdilaostephen@gmail.com>
2021-07-07 13:22:20 -0500

    Changed from file storage into cloud database

--- CoreC#/RestaurantReview/RRDL/Repository.cs
+++ CoreC#/RestaurantReview/RRDL/Repository.cs
@@ -3,36 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using RRModel;
+using System.Linq;
 
 namespace RRDL
 {
     public class Repository : IRepository
     {
-        private const string _filePath = "./../RRDL/Database/Restaurant.json";
-        private string _jsonString;
+        private Entities.DemoDbContext _context;
+        public Repository(Entities.DemoDbContext p_context)
+        {
+            _context = p_context;
+        }
         public Restaurant AddRestaurant(Restaurant p_rest)
         {
-            List<Restaurant> listOfRestaurants = this.GetAllRestaurant();
-            listOfRestaurants.Add(p_rest);
+            // List<Restaurant> listOfRestaurants = this.GetAllRestaurant();
+            // listOfRestaurants.Add(p_rest);
 
-            _jsonString = JsonSerializer.Serialize(listOfRestaurants, new JsonSerializerOptions{WriteIndented = true});
-            File.WriteAllText(_filePath, _jsonString);
-            return p_rest;
+            // _jsonString = JsonSerializer.Serialize(listOfRestaurants, new JsonSerializerOptions{WriteIndented = true});
+            // File.WriteAllText(_filePath, _jsonString);
+            // return p_rest;
+            throw new System.NotImplementedException();
         }
 
         public List<Restaurant> GetAllRestaurant()
         {
-            try
-            {
-                _jsonString = File.ReadAllText(_filePath);
-            }
-            catch (System.Exception)
-            {
-                throw new Exception("File path is invalid");
-            }
-
-            //This will return a list of restaurant from the jsonString that came from 
-            return JsonSerializer.Deserialize<List<Restaurant>>(_jsonString);
+            //Method Syntax way
+            return _context.Restaurants.Select(
+                rest => 
+                    new Restaurant()
+                    {
+                        Id = rest.Id,
+                        Name = rest.RestaurantName,
+                        City = rest.City,
+                        State = rest.RestaurantState
+                    }
+            ).ToList();
         }
 
         public Restaurant GetRestaurant(Restaurant p_rest)
