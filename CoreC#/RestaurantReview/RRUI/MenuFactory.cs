76eb3cf06fa930299d3213de075616f5f32f3944
Scifiler-sys <pagdilaostephen@gmail.com>
2021-07-07 13:23:06 -0500

    Added configuration builder for safety

--- CoreC#/RestaurantReview/RRUI/MenuFactory.cs
+++ CoreC#/RestaurantReview/RRUI/MenuFactory.cs
@@ -1,5 +1,9 @@
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using RRBL;
 using RRDL;
+using RRDL.Entities;
 
 namespace RRUI
 {
@@ -7,6 +11,20 @@ namespace RRUI
     {
         public IMenu GetMenu(MenuType p_menu)
         {
+
+            //Get the configuration from our appsetting.json file
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json")
+                .Build();
+
+            //Grabs our connectionString from our appsetting.json
+            string connectionString = configuration.GetConnectionString("Reference2DB");
+
+            DbContextOptions<DemoDbContext> options = new DbContextOptionsBuilder<DemoDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
             switch (p_menu)
             {
                 case MenuType.MainMenu:
@@ -17,9 +35,9 @@ namespace RRUI
                     //ShowRestaurantMenu needs a RestaurantBL object in the parameter because it depends on that object to be able to run its functionality
                     //RestaurantBL needs the Repository object in the parameter because it depends on that object to be able to run
                     //This is call Dependency Injection
-                    return new ShowRestaurantMenu(new RestaurantBL(new Repository()));
+                    return new ShowRestaurantMenu(new RestaurantBL(new Repository(new DemoDbContext(options))));
                 case MenuType.AddRestaurantMenu:
-                    return new AddRestaurantMenu(new RestaurantBL(new Repository()));
+                    return new AddRestaurantMenu(new RestaurantBL(new Repository(new DemoDbContext(options))));
                 default:
                     return null;
             }
