b81bc8084658c40fcc5de6f538dd02a2d0f09432
Scifiler-sys <pagdilaostephen@gmail.com>
2021-07-15 11:14:36 -0500

    Added Web App

---
+++ CoreC#/RestaurantReview/RRWebUI/Models/RestaurantVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RRWebUI.Models
+{
+    public class RestaurantVM
+    {
+        public RestaurantVM()
+        { }
+
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
