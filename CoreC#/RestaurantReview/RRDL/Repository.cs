0fa319986e49813aa1a5fa824249692be52a6601
Scifiler-sys <pagdilaostephen@gmail.com>
2021-07-15 11:14:11 -0500

    Did Code First Approach

--- CoreC#/RestaurantReview/RRDL/Repository.cs
+++ CoreC#/RestaurantReview/RRDL/Repository.cs
@@ -2,48 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using Model = RRModel;
-using Entity = RRDL.Entities;
+using RRModel;
 using System.Linq;
 
 namespace RRDL
 {
     public class Repository : IRepository
     {
-        private Entities.DemoDbContext _context;
-        public Repository(Entity.DemoDbContext p_context)
+        private RRDBContext _context;
+        public Repository(RRDBContext p_context)
         {
             _context = p_context;
         }
-        public Model.Restaurant AddRestaurant(Model.Restaurant p_rest)
+        public Restaurant AddRestaurant(Restaurant p_rest)
         {
-            _context.Restaurants.Add(new Entity.Restaurant{
-                Id = p_rest.Id,
-                RestaurantName = p_rest.Name,
-                RestaurantState = p_rest.State,
-                City = p_rest.City
-            });
-
+            _context.Restaurants.Add(p_rest);
             _context.SaveChanges();
             return p_rest;
         }
 
-        public List<Model.Restaurant> GetAllRestaurant()
+        public List<Restaurant> GetAllRestaurant()
         {
             //Method Syntax way
-            return _context.Restaurants.Select(
-                rest => 
-                    new Model.Restaurant()
-                    {
-                        Id = rest.Id,
-                        Name = rest.RestaurantName,
-                        City = rest.City,
-                        State = rest.RestaurantState
-                    }
-            ).ToList();
+            return _context.Restaurants.Select(rest => rest).ToList();
         }
 
-        public Model.Restaurant GetRestaurant(Model.Restaurant p_rest)
+        public Restaurant GetRestaurant(Restaurant p_rest)
         {
             throw new System.NotImplementedException();
         }
